a82c80509b8237b0b34a64b7f7bbdf4b2ca507b2
barabash_e <barabash.eugenia@palex.ru>
2022-06-14 16:55:35 +0700

    Task 18

---
+++ appManager/GroupHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace addressbook_tests_autoit
+{
+    public class GroupHelper : HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public GroupHelper(ApplicationManager manager) : base(manager) { }
+
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> list = new List<GroupData>();
+            OpenGroupsDialog();
+            string count = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51",
+                "GetItemCount", "#0", "");
+            for (int i = 0; i < int.Parse(count); i++)
+            {
+               string item =  aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51",
+                "GetText", "#0|#"+i, "");
+                list.Add(new GroupData()
+                {
+                    Name = item
+                });
+            }
+            CloseGroupsDialog();
+            return list;
+        }
+
+        public int GetGroupCount()
+        {
+            string count = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51",
+                "GetItemCount", "#0", "");
+            return int.Parse(count);
+        }
+
+        public GroupHelper Remove(int g)
+        {
+            OpenGroupsDialog();
+            SelectGroup(1);
+            DeleteGroup();
+            return this;
+        }
+
+        public GroupHelper SelectGroup(int index)
+        {
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51");
+            return this;
+        }
+
+        public GroupHelper DeleteGroup()
+        {
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d51");
+            aux.WinWait("Delete group");
+            aux.ControlClick("Delete group", "", "WindowsForms10.BUTTON.app.0.2c908d53");
+            return this;
+        }
+
+        public void Add(GroupData newGroup)
+        {
+            OpenGroupsDialog();
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d53");
+            aux.Send(newGroup.Name);
+            aux.Send("{ENTER}");
+            CloseGroupsDialog();
+        }
+
+        public void CloseGroupsDialog()
+        {
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d54");
+        }
+
+        public void OpenGroupsDialog()
+        {
+            aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d512");
+            aux.WinWait(GROUPWINTITLE);
+        }
+    }
+}
---
+++ tests/GroupRemovingTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupRemovingTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoving()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            app.Groups.Remove(1);
+
+            Assert.AreEqual(oldGroups.Count - 1, app.Groups.GetGroupCount());
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
